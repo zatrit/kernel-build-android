3134cb8d7fc6a9dd845479297cc2c6eada82de16
zatrit <zatrit@gmail.com>
2025-07-05 13:09:21 +0300

    build: implemented ccache caching

--- build.sh
+++ build.sh
@@ -7,15 +7,16 @@ if [ ! -d "$CLANG_DIR" ]; then
   error "ERROR: clang-$CLANG_PREBUILT is not installed. Use './setup-toolchain.sh'"
 fi
 
-USE_CCACHE=false
+use_ccache=false
 
 if command -v ccache >/dev/null 2>&1; then
   echo "Using ccache ($(which ccache))"
-  USE_CCACHE=true
+  use_ccache=true
 fi
 
 config_name="$1"
 sources_dir="$2"
+shift 2
 
 if [ -z "$config_name" ] || [ -z "$sources_dir" ]; then
   error "Usage: $0 [config name] [sources dir]"
@@ -27,11 +28,15 @@ fi
 
 cd "$sources_dir"
 
-build_env="LLVM=$CLANG_DIR/bin/ LLVM_IAS=1 O=$OUTPUT_DIR ARCH=arm64"
+cc="$CLANG_DIR/bin/clang"
+cxx="$CLANG_DIR/bin/clang++"
 
-if [ "$USE_CCACHE" = true ]; then
-    build_env="$build_env CC='ccache $CLANG_DIR/bin/clang' CXX='ccache $CLANG_DIR/bin/clang++'"
+if [ "$use_ccache" = true ]; then
+  cc="ccache $cc"
+  cxx="ccache $cxx"
 fi
+
+build_env="O=$OUTPUT_DIR LLVM=$CLANG_DIR/bin/ LLVM_IAS=1 ARCH=arm64"
 
 mkdir -p "$OUTPUT_DIR"
 make $build_env mrproper
@@ -39,4 +44,4 @@ make $build_env mrproper
 cp "$CONFIG_DIR/$config_name" "$OUTPUT_DIR/.config"
 
 make $build_env olddefconfig
-make $build_env
+make $build_env CC="$cc" CXX="$cxx" "$@"
